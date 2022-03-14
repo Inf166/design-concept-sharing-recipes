50e4c22d0adfc453083a99f9d0d58d62eb61cc0e
Joel Mai <38419872+Inf166@users.noreply.github.com>
2022-03-14 13:19:55 +0100

    [FEATURE] Add icons, images and json to clickdummy

--- Clickdummy/webpack.prod.js
+++ Clickdummy/webpack.prod.js
@@ -3,10 +3,15 @@ const { merge } = require('webpack-merge');
 
 const common = require("./webpack.common");
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "JavaScripts/main-[hash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+       new CleanWebpackPlugin(),
+  ]
 });
