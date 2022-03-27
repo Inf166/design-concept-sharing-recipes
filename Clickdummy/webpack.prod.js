cab455639d45522d734a6946ff857418b1b68efb
Joel Mai <38419872+Inf166@users.noreply.github.com>
2022-03-27 15:27:21 +0200

    [BUGFIX] Fix public path

--- Clickdummy/webpack.prod.js
+++ Clickdummy/webpack.prod.js
@@ -20,6 +20,7 @@ module.exports = merge(common, {
         options: {
           search: 'a href="/',
           replace: 'a href="/design-concept-sharing-recipes/',
+          flags: 'g',
         }
       },
     ]
