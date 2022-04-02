6ee5786f3786b22eadea1800ad009d4bd42973b2
Joel Mai <38419872+Inf166@users.noreply.github.com>
2022-04-02 16:38:52 +0200

    [FEATURE] Add draggable option to new recipe step 8

---
+++ Clickdummy/src/JavaScripts/04-organisms/sort.js
@@ -0,0 +1,4 @@
+import { Sortable } from 'sortablejs'; // https://github.com/SortableJS/Sortable
+
+var el = document.getElementById('ingredient-list');
+var sortable = Sortable.create(el);
--- Clickdummy/src/JavaScripts/index.js
+++ Clickdummy/src/JavaScripts/index.js
@@ -1,3 +1,12 @@
-import { alert } from "./01-generic/service-worker";
+import { Sortable } from 'sortablejs'; // https://github.com/SortableJS/Sortable
+if (document.getElementById('ingredient-list')) {
+  var el = document.getElementById('ingredient-list');
+  var sortable = Sortable.create(el, {
+    delay: 100, // time in milliseconds to define when the sorting should start
+    delayOnTouchOnly: true, // only delay if user is using touch
+    handle: ".draggable",  // Drag handle selector within list items
+    dragoverBubble: true,
+  });
+}
 
-alert();
+// TODO FIX EMPTIED INPUT FIELDS AFTER ADD NEW INGREDIENT
